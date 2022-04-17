42233b2c00021d007134e2bfada5914524061aab
gilhan <rlfgks97@gmail.com>
2022-04-17 18:18:10 +0900

    쓰지 않는 코드 삭제

--- backend/app_crawling/func_crawler/naver/nw_detail_scraper.py
+++ backend/app_crawling/func_crawler/naver/nw_detail_scraper.py
@@ -3,18 +3,9 @@ from bs4 import BeautifulSoup
 import json
 from collections import OrderedDict
 
-# f = open("/Users/yong-gilhan/Desktop/School/4-1/시종설/github/practice/for-gilhan/detail_link_list_of_naver.json", 'r')
-# with open('/Users/yong-gilhan/Desktop/School/4-1/시종설/github/practice/for-gilhan/detail_link_list_of_naver.json') as json_file:
-#     json_data = json.load(json_file)
-
-# print('1')
-# link_list = json_data["links"]
-# print('2')
-
 def get_details_webtoon_info(link_json):
 
 		def get_information(link):
-				print('4')
 				info = OrderedDict()
 
 				html = requests.get(link)
@@ -57,7 +48,6 @@ def get_details_webtoon_info(link_json):
 				return wt_list_json
 
 		def save_naver_wt(link_json):
-				print("link_json:", link_json)
 				webtoon = OrderedDict()
 				webtoon['Naver'] = json.loads(get_wt_info(link_json))
 				webtoon = json.dumps(webtoon, ensure_ascii=False, indent="\t")
