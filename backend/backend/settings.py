e563a6173f4a63b661e2d352cc9ed6af7951fe6b
Hannah <81863653+Hanna-511@users.noreply.github.com>
2022-05-11 03:57:31 +0900

    Update settings.py
    장고 리액트 연결

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,7 +55,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+]
 
+CORS_ALLOW_CREDENTIALS = True
 # ELASTICSEARCH_DSL={
 #     'default': {
 #         'hosts': 'elasticsearch:9200'
